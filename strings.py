5ac2f11fc2651b0a02de3e34b8e4845d4bf7ec65
franciscoDastres <franciscomeza.1234@gmail.com>
2025-11-25 00:48:16 -0300

    feat: :sparkles: adding new features in string by midu

--- strings.py
+++ strings.py
@@ -2,3 +2,5 @@ print("python es la hostia")
 print("python", "es" "la hostia")
 print("python","es","brutal", sep = "-") ## separador para la string
 print("python","es","brutal", sep = "*") ## separador para la string
+print("Esto se imprime" , end =  "! ")
+print("en una linea")
