88465bac6952b9e23f5b8a2639fb51b44028deed
franciscoDastres <franciscomeza.1234@gmail.com>
2025-11-25 00:15:19 -0300

    feat: :sparkles: adding new line in string about separation

--- strings.py
+++ strings.py
@@ -1,3 +1,4 @@
 print("python es la hostia")
 print("python", "es" "la hostia")
-print("python","es","brutal", sep = "-")
+print("python","es","brutal", sep = "-") ## separador para la string
+print("python","es","brutal", sep = "*") ## separador para la string
