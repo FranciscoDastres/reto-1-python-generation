f8902526d57ac846ff227b08d87afd3fd53ca2a6
franciscoDastres <franciscomeza.1234@gmail.com>
2025-11-25 12:36:07 -0300

    feat: :sparkles: adding array in py called dictionary list

--- loops.py
+++ loops.py
@@ -1,5 +1,13 @@
-fruits = ["apple", "banana", "cherry"]
+# fruits = ["apple", "banana", "cherry"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+fruits  = [
+    {"name":"apple"},
+    {"name":"banana"},
+    {"name":"cherry"}
+]
 
 for fruit in fruits:
-    print(fruit)
-
+    print(fruit["name"])
