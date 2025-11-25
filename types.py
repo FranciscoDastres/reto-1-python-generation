6d9a282ed230054b4171eead728f24f65b46e78a
franciscoDastres <franciscomeza.1234@gmail.com>
2025-11-25 03:27:21 -0300

    feat: :sparkles: adding new files types and variables

---
+++ types.py
@@ -0,0 +1,26 @@
+###
+# python tiene varios tipos de datos
+# int, float, complex, srt, bool, Nonetype , list, tuple, dict, range, set ... 
+###
+
+print( "int")
+print(type(10))
+print(type(0))
+print(type(-5))
+print(type(10))
+
+print("float:")
+print(type(3.14))
+print(type(1.0))
+print(type(1e3))
+
+print("complex:")
+print(type(1 + 2j))
+
+print("bool:")
+print(type(True))
+print(type(False))
+print(type(1 < 2))
+
+print("NoneType:")
+print(type(None))
---
+++ variables.py
@@ -0,0 +1,16 @@
+my_name = "midudev" 
+
+print(my_name)
+
+my_name ="dnth"
+
+print(my_name)
+
+# tipado dinamico  el tipo de dato se determina en tiempo de ejcucion
+# no tienes que declararlo explicitamente
+
+name = "dnth"
+print(type(name))
+
+name = 32
+print(type(name))
