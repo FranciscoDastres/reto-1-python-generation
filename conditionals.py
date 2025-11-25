f9e17b8c37745a99308816e7d1eae7dcd0b70555
franciscoDastres <franciscomeza.1234@gmail.com>
2025-11-25 12:17:25 -0300

    feat: :sparkles: modify conditioals adding moar elifs

--- conditionals.py
+++ conditionals.py
@@ -8,3 +8,14 @@ elif int(edad) == MAYOR_EDAD:
     print("Eres mayor de edad, tienes exactamente 18 años")
 else:
     print("Eres menor de edad")
+
+def definit_etapa_vida(edad):
+    if (edad) < 12:
+            print("Eres un niño")
+    elif (edad) < 18:
+            print("Eres un adulto")
+    elif (edad) < 25:
+            print("Eres un adulto joven")
+    elif (edad) < 60:
+            print("Eres un adulto mayor")
+
